5abea47790e35a51c4a458515b8e6f437ab469b7
Premnath K <premnathkulal1998@gmail.com>
2025-03-25 13:42:15 +0530

    Implemented footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopHeaderComponent } from './components/TopHeader/top-header/top-header.component';
+import { FooterComponent } from './components/Footer/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TopHeaderComponent],
+  imports: [RouterOutlet, TopHeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
