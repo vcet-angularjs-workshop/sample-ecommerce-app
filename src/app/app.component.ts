e759f34b0b43baf474aaaa6a9d625d1f128554af
Premnath K <premnathkulal1998@gmail.com>
2025-03-25 13:20:37 +0530

    Implemented top header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopHeaderComponent } from './components/TopHeader/top-header/top-header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
