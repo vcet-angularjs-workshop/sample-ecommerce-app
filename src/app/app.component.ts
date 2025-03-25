2eda49508b5c7eecdadfd31d989f148ae7936633
Premnath K <premnathkulal1998@gmail.com>
2025-03-25 16:10:56 +0530

    Implemented navigation bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TopHeaderComponent } from './components/TopHeader/top-header/top-header.component';
-import { FooterComponent } from './components/Footer/footer/footer.component';
+import { TopHeaderComponent } from './components/top-header/top-header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TopHeaderComponent, FooterComponent],
+  imports: [RouterOutlet, TopHeaderComponent, FooterComponent, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
