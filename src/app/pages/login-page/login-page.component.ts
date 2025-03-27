8838133ca65583b2b6abe24970383936012d98ce
Premnath K <premnathkulal1998@gmail.com>
2025-03-27 20:39:11 +0530

    Input handler added for login form

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-login-page',
   imports: [],
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.scss'
+  styleUrl: './login-page.component.scss',
 })
 export class LoginPageComponent {
+  userName = signal('');
+  password = signal('');
 
+  onUserNameInput(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    this.userName.set(inputElement.value);
+  }
+
+  onPasswordInput(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    this.password.set(inputElement.value);
+  }
+
+  handleLogin() {
+    console.log(this.userName(), this.password());
+  }
 }
