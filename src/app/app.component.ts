4ad20122d2b7bc94ea958ee0c7d8d42a5cd2adf2
Premnath K <premnathkulal1998@gmail.com>
2025-03-27 13:35:45 +0530

    Updated app router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,21 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { TopHeaderComponent } from './components/top-header/top-header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { CartPageComponent } from './pages/cart-page/cart-page.component';
-import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    TopHeaderComponent,
-    FooterComponent,
-    NavBarComponent,
-    HomePageComponent,
-    CartPageComponent,
-    LoginPageComponent,
-  ],
+  imports: [RouterOutlet, TopHeaderComponent, FooterComponent, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { CartPageComponent } from './pages/cart-page/cart-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomePageComponent,
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent,
+  },
+  {
+    path: 'cart',
+    component: CartPageComponent,
+  },
+];
